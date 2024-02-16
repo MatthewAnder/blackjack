517821851a4ffde74be95dcc49aac692f62185be
Matthew <mharyant@students.cs.ubc.ca>
2024-02-15 22:09:52 -0800

    Finish testing for user class

--- src/test/model/UserTest.java
+++ src/test/model/UserTest.java
@@ -3,7 +3,7 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
     User user;
@@ -14,8 +14,32 @@ public class UserTest {
     }
 
     @Test
+    public void testTakeMoney() {
+        assertEquals(100, user.takeMoney(100));
+        assertEquals(900, user.getMoney());
+    }
+
+    @Test
+    public void testGiveMoney() {
+        assertEquals(1000, user.getMoney());
+        user.giveMoney(1000);
+        assertEquals(2000, user.getMoney());
+    }
+
+    @Test
     public void testGetMoney() {
-        assertEquals(0, user.getMoney());
+        assertEquals(1000, user.getMoney());
+    }
+
+    @Test
+    public void testAddHand() {
+        Cards card1 = new Cards(Suits.HEART, Ranks.THREE);
+        Cards card2 = new Cards(Suits.CLUB, Ranks.KING);
+        user.addHand(card1);
+        user.addHand(card2);
+
+        assertEquals(card1, user.getHand().get(0));
+        assertEquals(card2, user.getHand().get(1));
     }
 
     @Test
@@ -32,5 +56,18 @@ public class UserTest {
         assertEquals(5, user.getValueOfHand());
         user.addHand(new Cards(Suits.HEART, Ranks.NINE));
         assertEquals(14, user.getValueOfHand());
+    }
+
+    @Test
+    public  void testResetHand() {
+        Cards card1 = new Cards(Suits.HEART, Ranks.THREE);
+        Cards card2 = new Cards(Suits.CLUB, Ranks.KING);
+        user.addHand(card1);
+        user.addHand(card2);
+
+        assertEquals(2, user.getHand().size());
+        user.resetHand();
+        assertEquals(0, user.getHand().size());
+        assertFalse(1 == user.getHand().size());
     }
 }
